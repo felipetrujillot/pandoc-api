c748ccfdf673559f3af2fdc8ef844b9bf219db25
Felipe <trujillo.felipe1997@gmail.com>
2024-09-09 20:50:23 -0300

    avance

--- main.py
+++ main.py
@@ -2,12 +2,33 @@ from fastapi import FastAPI, UploadFile, File,Query,Form
 import subprocess
 import shutil
 import os
+import pymupdf4llm
 import random
+from tempfile import NamedTemporaryFile
+
 app = FastAPI()
 
 @app.get("/")
 async def hello():
     return {"hello" : "world"}
+
+
+@app.post("/pdf-to-md/")
+async def convert_pdf_to_md(file: UploadFile = File(...)):
+
+    with NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        # Write the uploaded file content to the temp file
+        temp_file.write(await file.read())
+        temp_file_path = temp_file.name
+    
+    # Convert the temporary PDF file to Markdown
+    with open(temp_file_path, "rb") as pdf_file:
+        md_text = pymupdf4llm.to_markdown(pdf_file)
+    
+    # Optionally, delete the temporary file if it's no longer needed
+    os.remove(temp_file_path)
+
+    return {"markdown": md_text}
 
 @app.post("/docx-to-md/")
 async def convert_to_md(file: UploadFile = File(...)):
